ce5023a20010d014a756e66c971cfc1fd1b0e12e
Liam Martens <hi@liammartens.com>
2026-02-03 00:10:46 -0500

    fix: move update call inside loop

--- tsw-controller-mod/ue4ss-mod/src/dllmain.cpp
+++ tsw-controller-mod/ue4ss-mod/src/dllmain.cpp
@@ -418,14 +418,14 @@ class TSWControllerMod : public RC::CppUserModBase
                     TSWControllerMod::DIRECT_CONTROL_TARGET_STATE.erase(control_pair.first);
                 }
             }
-        }
 
-        /* run post update functions */
-        if (call_update_functions_func)
-        {
-            Output::send<LogLevel::Verbose>(STR("[TSWControllerMod] Calling M3 MTA specific CallUpdateFunctions\n"));
-            EmptyVoidFunctionParams params{};
-            drivable_actor_result.DrivableActor->ProcessEvent(call_update_functions_func, &params);
+            /* run post update functions */
+            if (call_update_functions_func)
+            {
+                Output::send<LogLevel::Verbose>(STR("[TSWControllerMod] Calling M3 MTA specific CallUpdateFunctions\n"));
+                EmptyVoidFunctionParams params{};
+                drivable_actor_result.DrivableActor->ProcessEvent(call_update_functions_func, &params);
+            }
         }
 
         direct_control_target_state_lock.unlock();
